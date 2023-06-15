6b68ab1958265c9c590f5bde4717ed2e5459e9eb
mmjobst <62211266+mmjobst@users.noreply.github.com>
2023-06-15 15:56:53 +0200

    Initial commit

---
+++ RoboDog/src/main/java/com/example/RoboDog/RoboDogApplication.java
@@ -0,0 +1,14 @@
+package com.example.RoboDog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoboDogApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(RoboDogApplication.class, args);
+	}
+
+}
---
+++ RoboDog/src/test/java/com/example/RoboDog/RoboDogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.RoboDog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoboDogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
